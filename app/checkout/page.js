24359aa53be8e930bc584b8706e7d3aca3324c5b
Omar_09 <brigades.anony99@gmail.com>
2024-03-06 22:57:57 +0100

    ok

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -4,10 +4,11 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import Page from './pagemain'; // Assuming Page is your component that uses Stripe functionality
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import { isCancel } from 'axios';
+import axios from 'axios';
 
-const PayPalButton = ({ productId, onSuccess, onError }) => {
-
+const PayPalButton = () => {
+  const [isCancel, setIsCancel] = useState(false)
+  const [isApproved, setIsApproved] = useState(false)
   const createOrder = async (data, actions) => {
     try {
       const response = await fetch('http://localhost:3002/process/paypal/api/orders', {
@@ -18,8 +19,10 @@ const PayPalButton = ({ productId, onSuccess, onError }) => {
         body: JSON.stringify({
           cart: [
             {
-              id: 1,
-              quantity: 1,
+              product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJGSUZBIDIzIiwicHJvZHVjdF9jYXRlZ29yeSI6IlBTNSIsInByaWNlIjoyNS41OSwicHJvZHVjdF9jdXJyZW5jeSI6IlVTRCIsInByb2R1Y3RfaWQiOjQ5LCJpYXQiOjE3MDk3NTY4NjF9.qsXpAH6A_anzlYztT6Rv58y4a6XZH59BbJW5Tmljrs4"
+            },
+            {
+              product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJjc2dvIiwicHJvZHVjdF9jYXRlZ29yeSI6IlBDIiwicHJpY2UiOjQ1LjU5LCJwcm9kdWN0X2N1cnJlbmN5IjoiVVNEIiwicHJvZHVjdF9pZCI6NTAsImlhdCI6MTcwOTc1Njg4Mn0.RmpDIFL4is26UNLAW-8wOsHO_2d9WXfx-7A_PR-voXQ"
             },
           ],
         }),
@@ -65,24 +68,37 @@ const PayPalButton = ({ productId, onSuccess, onError }) => {
     clientId: "AekJv-ARPKH-LoicG-ybAlfu2S1o6yZKKdK9rucpOsD1e11LDpt8Acn-eiXFuEgZDLp1D0DwHnVezb11"
   };
   return (
-    <PayPalScriptProvider options={initialOptions}>
-      <PayPalButtons onApprove={async (data, action) => {
-        try {
-          console.log(data);
-          response = await fetch(`http://localhost:3002/process/paypal/api/orders/capture`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              orderID: data.orderID
-            })
-          });
-        } catch (error) {
-          console.log(error.message);
-        }
-      }} createOrder={createOrder} onCancel={() => {alert("cancel order")}} />
-    </PayPalScriptProvider>
+    <div className=' h-screen w-full flex items-center flex-col justify-start gap-4'>
+      <div className={isApproved ? ' w-full  bg-green-600 flex items-center justify-center p-4' : "hidden"}>
+        <h1 className=' text-white text-xl text-center f'>you have seccesfully bought your product</h1>
+      </div>
+      <div className={isCancel ? ' w-full  bg-red-600 flex items-center justify-center p-4' : "hidden"}>
+        <h1 className=' text-white text-xl text-center f'>your order has been canceld</h1>
+      </div>
+      <div className=' h-52 flex items-center justify-center p-8'>
+        <PayPalScriptProvider options={initialOptions}>
+          <PayPalButtons style={{color: "blue", label: "pay"}} onApprove={async (data) => {
+            try {
+              const response = await fetch(`http://localhost:3002/process/paypal/api/orders/capture`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  orderID: data.orderID
+                })
+              });
+              console.log(response);
+              if (response.status === 201) {
+                setIsApproved(true)
+              }
+            } catch (error) {
+              console.log(error.message);
+            }
+          }} createOrder={createOrder} onCancel={() => { setIsCancel(true) }} />
+        </PayPalScriptProvider>
+      </div>
+    </div>
   );
 };
 
