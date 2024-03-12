d705d3829253f85132aab58e2c0d430eda69d53b
Omar_09 <brigades.anony99@gmail.com>
2024-03-12 16:55:50 +0100

    ok

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -5,11 +5,14 @@ import { loadStripe } from '@stripe/stripe-js';
 import Page from './pagemain'; // Assuming Page is your component that uses Stripe functionality
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const PayPalButton = () => {
   const [isCancel, setIsCancel] = useState(false)
   const [isError, setIsError] = useState(false)
   const [isApproved, setIsApproved] = useState(false)
+  const [isLoading,setIsLoading] = useState(false)
   const [products, setProducts] = useState([])
   const createOrder = async (data, actions) => {
     try {
@@ -21,9 +24,12 @@ const PayPalButton = () => {
         body: JSON.stringify({
           cart: [
             {
-              product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJDYWxsIG9mIGR1dHkgUHJpbWUiLCJwcm9kdWN0X2NhdGVnb3J5IjoiWGJveCBTfFgiLCJwcmljZSI6NTAwLjk5LCJwcm9kdWN0X2N1cnJlbmN5IjoiVVNEIiwicHJvZHVjdF9pZCI6NjMsImlhdCI6MTcxMDA5Mjc0MH0.jSYM1FcGHR-mnOhThv1HcIqCdAhawNkXb4Lg4OlVyWA"
+              product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJBdmF0YXIiLCJwcm9kdWN0X2NhdGVnb3J5IjoiUFM1IiwicHJpY2UiOjQ5Ljk5LCJwcm9kdWN0X2N1cnJlbmN5IjoiVVNEIiwicHJvZHVjdF9pZCI6NjUsInByb2R1Y3Rfa2V5IjoiVTJGc2RHVmtYMS9COWNCc2wwU09CM0tLUGZINDFsOExFSUNWd05Zd3ZBQjloNmE1WXhpLy9LVEJBZ2NVUm5EdiIsImlhdCI6MTcxMDI1NTQ4N30.NwqmovTLz5jDBhhyuzoPArr3tqa8cCm4HY8UcsfnSRc"
+            },
+            {
+              product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJBdmF0YXIgMiIsInByb2R1Y3RfY2F0ZWdvcnkiOiJQUzUiLCJwcmljZSI6MzkuOTksInByb2R1Y3RfY3VycmVuY3kiOiJVU0QiLCJwcm9kdWN0X2lkIjo2NiwicHJvZHVjdF9rZXkiOiJVMkZzZEdWa1gxK0MxVy9ESm8wanFlbjZjenNIaUdaY1ZFNUlacW5XTzlrWS9wM3E5VmZaN3FnWVhmMGY0MGc5IiwiaWF0IjoxNzEwMjU1NjEwfQ.iH-BvA6P6PeMxRyiOHuGrUPPugh01wpMoud7M1hX4Xg"
             }
-          ],
+          ]
         }),
       });
       const orderData = await response.json();
@@ -45,7 +51,6 @@ const PayPalButton = () => {
   const onApproveFun = async (data) => {
 
   }
-
   const initialOptions = {
     currency: "USD",
     intent: "capture",
@@ -53,6 +58,9 @@ const PayPalButton = () => {
   };
   return (
     <div className=' h-screen w-full flex items-center flex-col justify-start gap-4'>
+      <div  className={isLoading ? ' w-full' : "hidden"}>
+        <LinearProgress />
+      </div>
       <div className={isApproved ? ' w-full  bg-green-600 flex items-center justify-center p-4' : "hidden"}>
         <h1 className=' text-white text-xl text-center f'>you have seccesfully bought your product</h1>
       </div>
@@ -62,40 +70,54 @@ const PayPalButton = () => {
       <div className={isError ? ' w-full  bg-red-600 flex items-center justify-center p-4' : "hidden"}>
         <h1 className=' text-white text-xl text-center f'>your order has error something happend</h1>
       </div>
-      <div className=' h-52 flex items-center justify-center p-8'>
+      <div className={isLoading ? ' w-full bg-slate-400 flex items-center flex-col justify-center p-4' : "hidden"}>
+        <h1 className=' text-white text-xl text-center f'>Your order is under processing please be pation it may take a mineuts</h1>
+        <h1 className=' text-white text-xl text-center f'>Dont worry once it finished the product will be saved and sent to your email</h1>
+      </div>
+      <div className={isError || isLoading === true ? "hidden" :' h-52 flex items-center justify-center p-8'}>
         <PayPalScriptProvider options={initialOptions}>
           <PayPalButtons style={{ color: "blue", label: "pay" }} onApprove={async (data) => {
+            setIsLoading(true)
             try {
-            const cart = [
-              {
-                product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJDYWxsIG9mIGR1dHkgUHJpbWUiLCJwcm9kdWN0X2NhdGVnb3J5IjoiWGJveCBTfFgiLCJwcmljZSI6NTAwLjk5LCJwcm9kdWN0X2N1cnJlbmN5IjoiVVNEIiwicHJvZHVjdF9pZCI6NjMsImlhdCI6MTcxMDA5Mjc0MH0.jSYM1FcGHR-mnOhThv1HcIqCdAhawNkXb4Lg4OlVyWA"
-              }
-            ]
-            const payload = {
-              cart: cart,
-              orderID: data.orderID
-            };
-            const config = {
-              headers: {
-                'Content-Type': 'application/json',
-              }
-            };
-            axios.post("http://localhost:3002/process/paypal/api/orders/capture", payload, config)
-              .then((resp) => {
-                console.log(resp);
-                setIsApproved(true)
-                setProducts(resp.data.rest)
-              })
-              .catch((err) => {
-                console.log(err);
-                setIsError(true)
-              });
+              const cart = [
+                {
+                  product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJBdmF0YXIiLCJwcm9kdWN0X2NhdGVnb3J5IjoiUFM1IiwicHJpY2UiOjQ5Ljk5LCJwcm9kdWN0X2N1cnJlbmN5IjoiVVNEIiwicHJvZHVjdF9pZCI6NjUsInByb2R1Y3Rfa2V5IjoiVTJGc2RHVmtYMS9COWNCc2wwU09CM0tLUGZINDFsOExFSUNWd05Zd3ZBQjloNmE1WXhpLy9LVEJBZ2NVUm5EdiIsImlhdCI6MTcxMDI1NTQ4N30.NwqmovTLz5jDBhhyuzoPArr3tqa8cCm4HY8UcsfnSRc"
+                },
+                {
+                  product_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0X25hbWUiOiJBdmF0YXIgMiIsInByb2R1Y3RfY2F0ZWdvcnkiOiJQUzUiLCJwcmljZSI6MzkuOTksInByb2R1Y3RfY3VycmVuY3kiOiJVU0QiLCJwcm9kdWN0X2lkIjo2NiwicHJvZHVjdF9rZXkiOiJVMkZzZEdWa1gxK0MxVy9ESm8wanFlbjZjenNIaUdaY1ZFNUlacW5XTzlrWS9wM3E5VmZaN3FnWVhmMGY0MGc5IiwiaWF0IjoxNzEwMjU1NjEwfQ.iH-BvA6P6PeMxRyiOHuGrUPPugh01wpMoud7M1hX4Xg"
+                }
+              ]
+              const payload = {
+                cart: cart,
+                orderID: data.orderID
+              };
+              const bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9tYXJrZXJqZ0BnbWFpbC5jb20iLCJwYXNzd29yZCI6Im9tYXJrZXIwOSIsInVzZXJfaWQiOjEwMDMsInVzZXJuYW1lIjoib21hcmtlcjA5IiwiaWF0IjoxNzEwMjUyODk0LCJleHAiOjE3MTI4NDQ4OTR9.Wl1QmTHFqnqWPJXENtISYCWlzrvYeKHbz_zOfSBC7YY"
+              const config = {
+                headers: {
+                  'Content-Type': 'application/json',    // Setting the content type to JSON
+                  'Authorization': `Bearer ${bearerToken}`  // Including an Authorization header with a Bearer token
+                }
+              };
+              axios.post("http://localhost:3002/process/paypal/api/orders/capture", payload, config)
+                .then((resp) => {
+                  console.log(resp);
+                  setIsApproved(true)
+                  setProducts(resp.data.rest)
+                  setIsLoading(false)
+                  setIsError(false)
+                })
+                .catch((err) => {
+                  console.log(err);
+                  setIsError(true)
+                  setIsLoading(false)
+                });
             } catch {
               console.log("error payment")
             }
           }} createOrder={createOrder} onCancel={() => { setIsCancel(true) }} />
         </PayPalScriptProvider>
       </div>
+
       <div className={products.length === 0 ? "hidden" : ' w-full bg-blue-950 text-white flex gap-3 items-center justify-center p-3 flex-col'}>
         <h1>Your keys</h1>
         <div className=' flex items-center flex-col gap-5 justify-center'>
